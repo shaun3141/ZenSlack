a27e1b2cd1ca1f8b1191930981d50f3ffc141e99
Shaun VanWeelden <shaun.t.vanweelden@gmail.com>
2019-11-17 13:36:46 -0800

    Zendesk Functionality + Slack Hook Start

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json({limit: '10mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
 
-let client = zendeskSdk.createClient({
+let zClient = zendeskSdk.createClient({
   username:  'shaun.t.vanweelden@gmail.com',
   token:     process.env.ZENDESK_TOKEN,
   remoteUri: 'https://shaundev.zendesk.com/api/v2'
@@ -18,9 +18,13 @@ let client = zendeskSdk.createClient({
 
 let requesterEmail = "customer6@example.com";
 let requesterName = "Test6";
+let requesterUserId = '397057006634'
 
 let subject = "Hey now, it's working"; // TODO: Try with ' in the subject
 let body = "Care to celebrate?";
+let comment = "Commenting is fun";
+
+let ticketId = 14;
 
 // Good helper function :) 
 // console.log(JSON.stringify(result, null, 2, true));
@@ -33,19 +37,30 @@ let body = "Care to celebrate?";
     }
     // console.log("User:\n" + JSON.stringify(user, null, 2, true));
 
-    // ticket = await Zendesk.createTicketFromCustomerId(user.id, subject, body);
+    // ticket = await Zendesk.createTicketByCustomerId(user.id, subject, body);
     // console.log("Ticket:\n" + JSON.stringify(ticket, null, 2, true));
+
+    // let comments = await Zendesk.getPublicCommentsByTicketId(ticketId);
+    // console.log("Comments:\n" + JSON.stringify(comments, null, 2, true));
+
+    let result = await Zendesk.addCommentByTicketId(requesterUserId, 14, comment);
+    console.log("Result:\n" + JSON.stringify(result, null, 2, true));
 
   } catch (e) {
     console.log(e);
   }
 })();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello, World!'));
 
 app.post('/zendesk', (req, res) => {
   console.log(req.body);
-  res.status(200)
+  res.status(200).send();
+});
+
+app.post('/slack', (req, res) => {
+  console.log(req.body);
+  res.status(200).send();
 });
 
 app.listen(app.get('port'), () => console.log(`Example app listening on port ${app.get('port')}!`))
--- models/zendesk.js
+++ models/zendesk.js
@@ -32,39 +32,81 @@ exports.createCustomerByNameEmail = function(name, email) {
   return new Promise(function(resolve, reject) {
     // Create a User object
     let user = {
-      "user": {
-        "name": name, 
-        "email": email,
-        "verified": true
+      'user': {
+        'name': name, 
+        'email': email,
+        'verified': true
       }
     }
     client.users.create(user, function (err, req, result) {
       if (err) {
         reject(err);
       } else {
-        console.log("User Created with Id: " + result.id);
+        console.log('User Created with Id: ' + result.id);
         resolve(result);
       }
     });
   });
 }
 
-exports.createTicketFromCustomerId = function(userId, subject, message) {
+exports.createTicketByCustomerId = function(userId, subject, message) {
   return new Promise(function(resolve, reject) {
     // Create a new Ticket
     let newTicket = {
-      "ticket": {
-        "subject": subject,
-        "comment": {
-          "body": message
+      'ticket': {
+        'subject': subject,
+        'comment': {
+          'body': message
         },
-        'priority': "High",
+        'priority': 'High',
         'requester_id': userId,
-        'type': "Problem"
+        'type': 'Problem'
       }
     };
     
     client.tickets.create(newTicket, function (err, req, result) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+exports.getPublicCommentsByTicketId = function(ticketId) {
+  // TODO: The SDK uses the RequestAll method on this endpoint which should handle all pagination
+  // It's still weird to me that you need to get the first 'page' of results, which then has all results?
+  // Double-check this some time.
+  return new Promise(function(resolve, reject) {
+    client.tickets.getComments(ticketId, function (err, req, result) {
+      if (err) {
+        reject(err);
+      } else {
+        let commentsArray = result[0].comments.filter(val => {
+          return val.public;
+        });
+        resolve(commentsArray);
+      }
+    });
+  });
+}
+
+exports.addCommentByTicketId = function(userId, ticketId, comment) {
+  // Create Ticket Update
+  let ticketUpdate = {
+    'ticket': {
+      'id': ticketId,
+      'status': 'open',
+      'comment': {
+        'body': comment
+      },
+      'requester_id': userId,
+    }
+  };
+
+  return new Promise(function(resolve, reject) {
+    client.tickets.update(ticketId, ticketUpdate, function (err, req, result) {
       if (err) {
         reject(err);
       } else {
